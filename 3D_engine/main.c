b960ef92e0cf7edb200a20bd28bb85eeeceaf03b
SerialLooser <haroldducept@gmail.com>
2016-06-19 11:05:47 +0200

    Load texture

--- 3D_engine/main.c
+++ 3D_engine/main.c
@@ -4,8 +4,7 @@
 
 int main(int argc, char **argv)
 {
-    SDL_Window *window = NULL;
-    SDL_Event event;
+    
     bool end;
     
     // Initialisation de la SDL
@@ -18,25 +17,57 @@ int main(int argc, char **argv)
         return -1;
     }
     
-    window = SDL_CreateWindow("3D_engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("3D_engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
     
-    if(window == 0)
+    if(win == 0)
     {
-        printf("Erreur lors de la création de la fenetre: %s\n", SDL_GetError());
+        printf("SDL_CreateWindow error: %s\n", SDL_GetError());
         SDL_Quit();
         return -1;
     }
-    while(!end)
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(ren == NULL)
     {
-        SDL_WaitEvent(&event);
-        if(event.window.event == SDL_WINDOWEVENT_CLOSE)
-        {
-            end = true;
-        }
+        SDL_DestroyWindow(win);
+        printf("SDL_CreateRenderer error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Surface *bmp = SDL_LoadBMP("image.bmp");
+    if(bmp == NULL)
+    {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        printf("SDL_LoadBMP error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+    SDL_FreeSurface(bmp);
+    if(tex == NULL)
+    {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        printf("SDL_CreateTextureFromSurface error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    for(int i = 0; i < 3; i++)
+    {
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, tex, NULL, NULL);
+        SDL_RenderPresent(ren);
+        SDL_Delay(1000);
     }
     
     // On quitte la SDL
-    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
     SDL_Quit();
     
     return 0;
