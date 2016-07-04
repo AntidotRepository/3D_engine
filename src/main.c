9366f5f5f10eccf6913640d5345e6f7f4c9cfdab
SerialLooser <haroldducept@gmail.com>
2016-07-04 22:36:14 +0200

    Draw a 3d cube (displaying bug)

--- src/main.c
+++ src/main.c
@@ -2,8 +2,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define SCREEN_WIDTH    640                                                     // Window width
-#define SCREEN_HEIGHT   480                                                     // Window heigh
+#define SCREEN_WIDTH            640                                             // Window width
+#define SCREEN_HEIGHT           480                                             // Window heigh
+#define FRAME_RATE              50
+#define DEG_TO_RAD              0.01745329
+
+#define CONVERT_POS_X(pos_X)    pos_X+SCREEN_WIDTH/2
+#define CONVERT_POS_Y(pos_Y)    pos_Y+SCREEN_HEIGHT/2
+
+#define USER_DISTANCE           300
+
+typedef struct mPoint mPoint;
+struct mPoint
+{
+    float x;
+    float y;
+    float z;
+    char display;
+    float distance_to_eye;
+};
 
 //#error http://www.willusher.io/sdl2%20tutorials/2013/08/18/lesson-3-sdl-extension-libraries
 
@@ -15,10 +32,11 @@
 void drawTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3)
 {
     int xL, xC, xR, yL, yC, yR = 0;
-    
+    //printf("x1=%d, y1=%d, x2=%d, y2=%d, x3=%d, y3=%d\n", x1, y1, x2, y2, x3, y3);
     // We sort points from left to right
     if((x1 <= x2) && (x1 <= x3))
     {
+        printf("cas 1\n");
         xL = x1;
         yL = y1;
         if(x2 < x3)
@@ -38,6 +56,7 @@ void drawTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3
     }
     else if((x2 <= x3) && (x2 <= x1))
     {
+        printf("cas 2\n");
         xL = x2;
         yL = y2;
         if(x1 < x3)
@@ -57,6 +76,7 @@ void drawTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3
     }
     else
     {
+        printf("cas 3\n");
         xL = x3;
         yL = y3;
         if(x2 < x1)
@@ -64,7 +84,7 @@ void drawTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3
             xC = x2;
             yC = y2;
             xR = x1;
-            yR = x1;
+            yR = y1;
         }
         else
         {
@@ -96,6 +116,7 @@ void drawTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3
              y2 = (int)((double)(coef3*(i-xC+xL)+yC));                          // Calculate the end of the line for the second part
          }
          SDL_RenderDrawLine(renderer, i+xL, y1, i+xL, y2);                      // Draw a line filling the triangle
+         //printf("x=%d, y=%d, x=%d, y=%d\n", i+xL, y1, i+xL, y2);
      }
 }
 
@@ -111,6 +132,46 @@ void logSDLError(char *msg)
 
 int main(int argc, char **argv)
 {
+    int x_screen[8],y_screen[8];
+    int angle = 0;
+    mPoint tabPoint[8];
+    mPoint tabPointRotX[8];
+    mPoint tabPointRotY[8];
+    mPoint tabPointRotZ[8];
+    
+    tabPoint[0].x =  50;
+    tabPoint[0].y =  50;
+    tabPoint[0].z = -50;
+    
+    tabPoint[1].x = -50;
+    tabPoint[1].y =  50;
+    tabPoint[1].z = -50;
+    
+    tabPoint[2].x = -50;
+    tabPoint[2].y = -50;
+    tabPoint[2].z = -50;
+    
+    tabPoint[3].x =  50;
+    tabPoint[3].y = -50;
+    tabPoint[3].z = -50;
+    
+    tabPoint[4].x =  50;
+    tabPoint[4].y =  50;
+    tabPoint[4].z =  50;
+    
+    tabPoint[5].x = -50;
+    tabPoint[5].y =  50;
+    tabPoint[5].z =  50;
+    
+    tabPoint[6].x = -50;
+    tabPoint[6].y = -50;
+    tabPoint[6].z =  50;
+    
+    tabPoint[7].x =  50;
+    tabPoint[7].y = -50;
+    tabPoint[7].z =  50;
+    
+    
     if(SDL_Init(SDL_INIT_VIDEO) < 0)                                            // SDL Initialization
     {
         logSDLError("SDL_Init");
@@ -155,8 +216,29 @@ int main(int argc, char **argv)
     
     SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);                                  // We will draw in black
     
-    for(int i = 0; i<5000/20; i++)
+    while(1)
     {
+        angle += 1;
+        
+        for(int i = 0; i<8; i++)
+        {
+            tabPointRotX[i].x = tabPoint[i].x*cos(angle%360*DEG_TO_RAD)-tabPoint[i].y*sin(angle%360*DEG_TO_RAD);
+            tabPointRotX[i].y = tabPoint[i].x*sin(angle%360*DEG_TO_RAD)+tabPoint[i].y*cos(angle%360*DEG_TO_RAD);
+            tabPointRotX[i].z = tabPoint[i].z;
+        }
+        
+        for(int i = 0; i<8; i++)
+        {
+            tabPointRotY[i].x = tabPointRotX[i].x;
+            tabPointRotY[i].y = tabPointRotX[i].y*cos(angle%360*DEG_TO_RAD)-tabPointRotX[i].z*sin(angle%360*DEG_TO_RAD);
+            tabPointRotY[i].z = tabPointRotX[i].y*sin(angle%360*DEG_TO_RAD)+tabPointRotX[i].z*cos(angle%360*DEG_TO_RAD);
+        }
+        
+        for(int i = 0; i<8; i++)
+        {
+            x_screen[i] = CONVERT_POS_X((USER_DISTANCE*tabPointRotY[i].x)/(USER_DISTANCE+tabPointRotY[i].z));
+            y_screen[i] = CONVERT_POS_Y((USER_DISTANCE*tabPointRotY[i].y)/(USER_DISTANCE+tabPointRotY[i].z));
+        }
         SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);                        // We set the background color (white)
         SDL_RenderClear(ren);
         
@@ -168,9 +250,31 @@ int main(int argc, char **argv)
         SDL_RenderDrawLine(ren, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, 0, SCREEN_HEIGHT-1);
         SDL_RenderDrawLine(ren, 0, SCREEN_HEIGHT-1, 0, 0);
         
-        drawTriangle(ren, 10, SCREEN_HEIGHT-10, SCREEN_WIDTH-10, SCREEN_HEIGHT-10, SCREEN_WIDTH/2, 10);
+        drawTriangle(ren, x_screen[0], y_screen[0], x_screen[1], y_screen[1], x_screen[2], y_screen[2]);
+        drawTriangle(ren, x_screen[2], y_screen[2], x_screen[3], y_screen[3], x_screen[0], y_screen[0]);
+        
+        SDL_SetRenderDrawColor(ren, 50, 50, 50, 255);                              // We will draw in black
+        drawTriangle(ren, x_screen[4], y_screen[4], x_screen[5], y_screen[5], x_screen[6], y_screen[6]);
+        drawTriangle(ren, x_screen[6], y_screen[6], x_screen[7], y_screen[7], x_screen[4], y_screen[4]);
+        
+        SDL_SetRenderDrawColor(ren, 100, 100, 100, 255);                              // We will draw in black
+        drawTriangle(ren, x_screen[5], y_screen[5], x_screen[6], y_screen[6], x_screen[2], y_screen[2]);
+        drawTriangle(ren, x_screen[2], y_screen[2], x_screen[1], y_screen[1], x_screen[5], y_screen[5]);
+        
+        SDL_SetRenderDrawColor(ren, 150, 150, 150, 255);                              // We will draw in black
+        drawTriangle(ren, x_screen[3], y_screen[3], x_screen[2], y_screen[2], x_screen[6], y_screen[6]);
+        drawTriangle(ren, x_screen[3], y_screen[3], x_screen[6], y_screen[6], x_screen[7], y_screen[7]);
+        
+        SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);                              // We will draw in black
+        drawTriangle(ren, x_screen[3], y_screen[3], x_screen[0], y_screen[0], x_screen[7], y_screen[7]);
+        drawTriangle(ren, x_screen[0], y_screen[0], x_screen[4], y_screen[4], x_screen[7], y_screen[7]);
+        
+        SDL_SetRenderDrawColor(ren, 225, 225, 225, 255);                              // We will draw in black
+        drawTriangle(ren, x_screen[5], y_screen[5], x_screen[4], y_screen[4], x_screen[0], y_screen[0]);
+        drawTriangle(ren, x_screen[0], y_screen[0], x_screen[1], y_screen[1], x_screen[5], y_screen[5]);
+        
         SDL_RenderPresent(ren);
-        SDL_Delay(20);
+        SDL_Delay(1000/FRAME_RATE);
     }
     
     SDL_DestroyRenderer(ren);
