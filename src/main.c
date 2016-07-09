acd82ee8da8cd2a0cfc7a924ac11844083fa465f
SerialLooser <haroldducept@gmail.com>
2016-07-09 22:04:38 +0200

    Changed  draw2DTriangle prototype and resolve displaying infinite line bug

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 #define SCREEN_WIDTH            640                                             // Window width
 #define SCREEN_HEIGHT           480                                             // Window heigh
-#define FRAME_RATE              10
+#define FRAME_RATE              50
 #define DEG_TO_RAD              0.01745329
 
 #define CONVERT_POS_X(pos_X)    pos_X+SCREEN_WIDTH/2
@@ -31,93 +31,99 @@ struct mPoint
  *  \brief      Draw a triangle
  *  \param[in]  Coordinates of points of triangle
  */
-void draw2DTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3)
+void draw2DTriangle(SDL_Renderer *renderer, mPoint p1, mPoint p2, mPoint p3)
 {
-    int xL, xC, xR, yL, yC, yR = 0;
-    //printf("x1=%d, y1=%d, x2=%d, y2=%d, x3=%d, y3=%d\n", x1, y1, x2, y2, x3, y3);
+    mPoint Left, Center, Right;
+    
     // We sort points from left to right
-    if((x1 <= x2) && (x1 <= x3))
+    if((p1.x_2D <= p2.x_2D) && (p1.x_2D <= p3.x_2D))
     {
-        xL = x1;
-        yL = y1;
-        if(x2 < x3)
+        Left = p1;
+        if(p2.x_2D < p3.x_2D)
         {
-            xC = x2;
-            yC = y2;
-            xR = x3;
-            yR = y3;
+            Center = p2;
+            Right = p3;
         }
         else
         {
-            xC = x3;
-            yC = y3;
-            xR = x2;
-            yR = y2;
+            Center = p3;
+            Right = p2;
         }
     }
-    else if((x2 <= x3) && (x2 <= x1))
+    else if((p2.x_2D <= p3.x_2D) && (p2.x_2D <= p1.x_2D))
     {
-        xL = x2;
-        yL = y2;
-        if(x1 < x3)
+        Left = p2;
+        if(p1.x_2D < p3.x_2D)
         {
-            xC = x1;
-            yC = y1;
-            xR = x3;
-            yR = y3;
+            Center = p1;
+            Right = p3;
         }
         else
         {
-            xC = x3;
-            yC = y3;
-            xR = x1;
-            yR = y1;
+            Center = p3;
+            Right = p1;
         }
     }
     else
     {
-        xL = x3;
-        yL = y3;
-        if(x2 < x1)
+        Left = p3;
+        if(p2.x_2D < p1.x_2D)
         {
-            xC = x2;
-            yC = y2;
-            xR = x1;
-            yR = y1;
+            Center = p2;
+            Right = p1;
         }
         else
         {
-            xC = x1;
-            yC = y1;
-            xR = x2;
-            yR = y2;
+            Center = p1;
+            Right = p2;
         }
     }
-
+    
     // We calculate coef of each side of the triangle
-    float coef1 = ((float)yC-(float)yL)/((float)xC-(float)xL);
-    float coef2 = ((float)yR-(float)yL)/((float)xR-(float)xL);
-    float coef3 = ((float)yR-(float)yC)/((float)xR-(float)xC);
+    float coef1 = 0.0, coef2 = 0.0, coef3 = 0.0;
+    if(Center.x_2D != Left.x_2D)
+        coef1 = ((float)Center.y_2D-(float)Left.y_2D)/((float)Center.x_2D-(float)Left.x_2D);
+    if(Right.x_2D != Left.x_2D)
+        coef2 = ((float)Right.y_2D-(float)Left.y_2D)/((float)Right.x_2D-(float)Left.x_2D);
+    if(Right.x_2D != Center.x_2D)
+        coef3 = ((float)Right.y_2D-(float)Center.y_2D)/((float)Right.x_2D-(float)Center.x_2D);
     
     // We start to fill it
-     for (int i = 0; i<(xR-xL); i++)
-     {
-         int y1 = 0;
-         int y2 = 0;
-         
-         y1 = (int)((double)(coef2*i))+yL;                                      // Calculate the beginning of the line
-         if(i <= xC-xL)
-         {
-             y2 = (int)((double)(coef1*i)+yL);                                  // Calculate the end of the line for the first part
-         }
-         else
-         {
-             y2 = (int)((double)(coef3*(i-xC+xL)+yC));                          // Calculate the end of the line for the second part
-         }
-         SDL_RenderDrawLine(renderer, i+xL, y1, i+xL, y2);                      // Draw a line filling the triangle
-         //printf("x=%d, y=%d, x=%d, y=%d\n", i+xL, y1, i+xL, y2);
-     }
+    for (int i = 0; i<(Right.x_2D-Left.x_2D); i++)
+    {
+        int y1 = 0;
+        int y2 = 0;
+        int ytemp = 0;
+        
+        y1 = (int)((double)(coef2*i))+Left.y_2D;                                // Calculate the beginning of the line
+        if(i <= Center.x_2D-Left.x_2D)
+        {
+            y2 = (int)((double)(coef1*i)+Left.y_2D);                            // Calculate the end of the line for the first part
+        }
+        else
+        {
+            y2 = (int)((double)(coef3*(i-Center.x_2D+Left.x_2D)+Center.y_2D));  // Calculate the end of the line for the second part
+        }
+        if(y1 >= y2)
+        {
+            ytemp = y1;
+            y1 = y2;
+            y2 = ytemp;
+        }
+        for(int j = y1; j<y2; j++)                                              // Draw the whole line
+        {
+            // ToDo
+            // Calculer distance à Left
+            //screen[i+(int)Left.x_2D][j].x_3D = ;
+            // Calculer distance à Center
+            // Calculer distance à Right
+            // Appliquer la fraction de la distance à chaque point sur son poids
+            //screen[i+Left.x_2D][j].distance_to_eye = 1.0;//sqrt(pow(,2));
+            SDL_RenderDrawPoint(renderer, i+Left.x_2D, j);
+        }
+    }
 }
+
 
 /**
  *  \fn     logSDLError
@@ -131,7 +137,6 @@ void logSDLError(char *msg)
 
 int main(int argc, char **argv)
 {
-    //int tabPointRotY[i].x_2D[8],tabPointRotY[i].y_2D[8];
     int angle = 0;
     float zBuffer[SCREEN_WIDTH][SCREEN_HEIGHT] = {{0}, {0}};
     
@@ -252,28 +257,28 @@ int main(int argc, char **argv)
         SDL_RenderDrawLine(ren, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, 0, SCREEN_HEIGHT-1);
         SDL_RenderDrawLine(ren, 0, SCREEN_HEIGHT-1, 0, 0);
         
-        draw2DTriangle(ren, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D, tabPointRotY[1].x_2D, tabPointRotY[1].y_2D, tabPointRotY[2].x_2D, tabPointRotY[2].y_2D);
-        draw2DTriangle(ren, tabPointRotY[2].x_2D, tabPointRotY[2].y_2D, tabPointRotY[3].x_2D, tabPointRotY[3].y_2D, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D);
+        draw2DTriangle(ren, tabPointRotY[0], tabPointRotY[1], tabPointRotY[2]);
+        draw2DTriangle(ren, tabPointRotY[2], tabPointRotY[3], tabPointRotY[0]);
         
         SDL_SetRenderDrawColor(ren, 50, 50, 50, 255);                              // We will draw in black
-        draw2DTriangle(ren, tabPointRotY[4].x_2D, tabPointRotY[4].y_2D, tabPointRotY[5].x_2D, tabPointRotY[5].y_2D, tabPointRotY[6].x_2D, tabPointRotY[6].y_2D);
-        draw2DTriangle(ren, tabPointRotY[6].x_2D, tabPointRotY[6].y_2D, tabPointRotY[7].x_2D, tabPointRotY[7].y_2D, tabPointRotY[4].x_2D, tabPointRotY[4].y_2D);
+        draw2DTriangle(ren, tabPointRotY[4], tabPointRotY[5], tabPointRotY[6]);
+        draw2DTriangle(ren, tabPointRotY[6], tabPointRotY[7], tabPointRotY[4]);
         
         SDL_SetRenderDrawColor(ren, 100, 100, 100, 255);                              // We will draw in black
-        draw2DTriangle(ren, tabPointRotY[5].x_2D, tabPointRotY[5].y_2D, tabPointRotY[6].x_2D, tabPointRotY[6].y_2D, tabPointRotY[2].x_2D, tabPointRotY[2].y_2D);
-        draw2DTriangle(ren, tabPointRotY[2].x_2D, tabPointRotY[2].y_2D, tabPointRotY[1].x_2D, tabPointRotY[1].y_2D, tabPointRotY[5].x_2D, tabPointRotY[5].y_2D);
+        draw2DTriangle(ren, tabPointRotY[5], tabPointRotY[6], tabPointRotY[2]);
+        draw2DTriangle(ren, tabPointRotY[2], tabPointRotY[1], tabPointRotY[5]);
         
         SDL_SetRenderDrawColor(ren, 150, 150, 150, 255);                              // We will draw in black
-        draw2DTriangle(ren, tabPointRotY[3].x_2D, tabPointRotY[3].y_2D, tabPointRotY[2].x_2D, tabPointRotY[2].y_2D, tabPointRotY[6].x_2D, tabPointRotY[6].y_2D);
-        draw2DTriangle(ren, tabPointRotY[3].x_2D, tabPointRotY[3].y_2D, tabPointRotY[6].x_2D, tabPointRotY[6].y_2D, tabPointRotY[7].x_2D, tabPointRotY[7].y_2D);
+        draw2DTriangle(ren, tabPointRotY[3], tabPointRotY[2], tabPointRotY[6]);
+        draw2DTriangle(ren, tabPointRotY[3], tabPointRotY[6], tabPointRotY[7]);
         
         SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);                              // We will draw in black
-        draw2DTriangle(ren, tabPointRotY[3].x_2D, tabPointRotY[3].y_2D, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D, tabPointRotY[7].x_2D, tabPointRotY[7].y_2D);
-        draw2DTriangle(ren, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D, tabPointRotY[4].x_2D, tabPointRotY[4].y_2D, tabPointRotY[7].x_2D, tabPointRotY[7].y_2D);
+        draw2DTriangle(ren, tabPointRotY[3], tabPointRotY[0], tabPointRotY[7]);
+        draw2DTriangle(ren, tabPointRotY[0], tabPointRotY[4], tabPointRotY[7]);
         
         SDL_SetRenderDrawColor(ren, 225, 225, 225, 255);                              // We will draw in black
-        draw2DTriangle(ren, tabPointRotY[5].x_2D, tabPointRotY[5].y_2D, tabPointRotY[4].x_2D, tabPointRotY[4].y_2D, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D);
-        draw2DTriangle(ren, tabPointRotY[0].x_2D, tabPointRotY[0].y_2D, tabPointRotY[1].x_2D, tabPointRotY[1].y_2D, tabPointRotY[5].x_2D, tabPointRotY[5].y_2D);
+        draw2DTriangle(ren, tabPointRotY[5], tabPointRotY[4], tabPointRotY[0]);
+        draw2DTriangle(ren, tabPointRotY[0], tabPointRotY[1], tabPointRotY[5]);
         
         SDL_RenderPresent(ren);
         SDL_Delay(1000/FRAME_RATE);
