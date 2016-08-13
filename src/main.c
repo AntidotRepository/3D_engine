e10be7db25507c1b22d0c0106a92101184bf60a5
SerialLooser <haroldducept@gmail.com>
2016-08-13 13:04:13 +0200

    Changed 3rd order matrix by 4th order

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 #define SCREEN_WIDTH            640                                             // Window width
 #define SCREEN_HEIGHT           480                                             // Window heigh
-#define FRAME_RATE              50
+#define FRAME_RATE              60
 #define DEG_TO_RAD              0.01745329
 #define MAX_DEPTH               1000
 
@@ -197,30 +197,50 @@ void calc_coef_plan(plan *p)
     p->coefD = (-1)*(p->coefA*p->pt1->x_3D + p->coefB*p->pt1->y_3D + p->coefC*p->pt1->z_3D);
 }
 
-void matrix_product(float res[3], float mat1[3][3], float mat2[3])
+/**
+ *  \fn         matrix_product
+ *  \brief      Multiplication of a 4x4 matrix by a 4-elements vector
+ *  \param[out] res result of the calculation
+ *  \param[in]  mat1 4x4 matrix
+ *  \param[in]  mat2 4-elements vector
+ */
+void matrix_product(float res[4], float mat1[4][4], float mat2[4])
 {
-    res[0] = mat1[0][0]*mat2[0]+mat1[0][1]*mat2[1]+mat1[0][2]*mat2[2];
-    res[1] = mat1[1][0]*mat2[0]+mat1[1][1]*mat2[1]+mat1[1][2]*mat2[2];
-    res[2] = mat1[2][0]*mat2[0]+mat1[2][1]*mat2[1]+mat1[2][2]*mat2[2];
+    res[0] = mat1[0][0]*mat2[0]+mat1[0][1]*mat2[1]+mat1[0][2]*mat2[2]+mat1[0][3]*mat2[3];
+    res[1] = mat1[1][0]*mat2[0]+mat1[1][1]*mat2[1]+mat1[1][2]*mat2[2]+mat1[1][3]*mat2[3];
+    res[2] = mat1[2][0]*mat2[0]+mat1[2][1]*mat2[1]+mat1[2][2]*mat2[2]+mat1[2][3]*mat2[3];
+    res[3] = mat1[3][0]*mat2[0]+mat1[3][1]*mat2[1]+mat1[3][2]*mat2[2]+mat1[3][3]*mat2[3];
 }
 
+/**
+ *  \fn             rotate
+ *  \brief          Rotate a point p by an angle gived by "angle" around an axis gived by x, y and z
+ *  \param[in]      x, y, z Unity axis of rotation
+ *  \param[in]      angle Angle to rotate
+ *  \param[inout]   *p Point to rotate
+ */
 void rotate(float x, float y, float z, int angle, m3DPoint *p)
 {
     float teta = angle % 360 * DEG_TO_RAD;
-    float mat1[3][3] =
-    {{x*x*(1-cos(teta))+cos(teta), x*y*(1-cos(teta))-z*sin(teta), x*z*(1-cos(teta))+y*sin(teta)},
-        {x*y*(1-cos(teta))+z*sin(teta), y*y*(1-cos(teta))+cos(teta), y*z*(1-cos(teta))-x*sin(teta)},
-        {x*z*(1-cos(teta))-y*sin(teta), y*z*(1-cos(teta))+x*sin(teta), z*z*(1-cos(teta))+cos(teta)}};
-    float mat2[3] = {p->x_3D, p->y_3D, p->z_3D};
-    float res[3] = {0};
+    float mat1[4][4] =
+    {{x*x*(1-cos(teta))+cos(teta), x*y*(1-cos(teta))-z*sin(teta), x*z*(1-cos(teta))+y*sin(teta), 0},
+        {x*y*(1-cos(teta))+z*sin(teta), y*y*(1-cos(teta))+cos(teta), y*z*(1-cos(teta))-x*sin(teta), 0},
+        {x*z*(1-cos(teta))-y*sin(teta), y*z*(1-cos(teta))+x*sin(teta), z*z*(1-cos(teta))+cos(teta), 0},
+        {0, 0, 0, 1}};
+    float mat2[4] = {p->x_3D, p->y_3D, p->z_3D, 1};
+    float res[4] = {0};
     matrix_product(res, mat1, mat2);
     
     p->x_3D = res[0];
     p->y_3D = res[1];
     p->z_3D = res[2];
-    
-    //return point;
 }
+
+void translate(float x, float y, float z, m3DPoint *p)
+{
+
+}
+
 
 /**
  *  \fn     logSDLError
@@ -234,6 +254,9 @@ void logSDLError(char *msg)
 
 int main(int argc, char **argv)
 {
+    Uint32 Actual_time = 0;
+    Uint32 Previous_time = 0;
+    
     int angle = 0;
     struct m3DPoint *canevas = malloc(SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(struct m3DPoint));  // Have to use dynamic allocation 'cause of to big table!
     TTF_Font *police = NULL;
@@ -243,39 +266,40 @@ int main(int argc, char **argv)
     camera.posY = 300;
     camera.posZ = 0;
     
-    m3DPoint tabPoint[8];
+    m3DPoint model[8];
+    m3DPoint view[8];
     
-    tabPoint[0].x_3D =  50;
-    tabPoint[0].y_3D =  50;
-    tabPoint[0].z_3D = -50;
+    model[0].x_3D =  50;
+    model[0].y_3D =  50;
+    model[0].z_3D = -50;
     
-    tabPoint[1].x_3D = -50;
-    tabPoint[1].y_3D =  50;
-    tabPoint[1].z_3D = -50;
+    model[1].x_3D = -50;
+    model[1].y_3D =  50;
+    model[1].z_3D = -50;
     
-    tabPoint[2].x_3D = -50;
-    tabPoint[2].y_3D = -50;
-    tabPoint[2].z_3D = -50;
+    model[2].x_3D = -50;
+    model[2].y_3D = -50;
+    model[2].z_3D = -50;
     
-    tabPoint[3].x_3D =  50;
-    tabPoint[3].y_3D = -50;
-    tabPoint[3].z_3D = -50;
+    model[3].x_3D =  50;
+    model[3].y_3D = -50;
+    model[3].z_3D = -50;
     
-    tabPoint[4].x_3D =  50;
-    tabPoint[4].y_3D =  50;
-    tabPoint[4].z_3D =  50;
+    model[4].x_3D =  50;
+    model[4].y_3D =  50;
+    model[4].z_3D =  50;
     
-    tabPoint[5].x_3D = -50;
-    tabPoint[5].y_3D =  50;
-    tabPoint[5].z_3D =  50;
+    model[5].x_3D = -50;
+    model[5].y_3D =  50;
+    model[5].z_3D =  50;
     
-    tabPoint[6].x_3D = -50;
-    tabPoint[6].y_3D = -50;
-    tabPoint[6].z_3D =  50;
+    model[6].x_3D = -50;
+    model[6].y_3D = -50;
+    model[6].z_3D =  50;
     
-    tabPoint[7].x_3D =  50;
-    tabPoint[7].y_3D = -50;
-    tabPoint[7].z_3D =  50;
+    model[7].x_3D =  50;
+    model[7].y_3D = -50;
+    model[7].z_3D =  50;
     
     plan plan1;
     plan plan2;
@@ -290,86 +314,86 @@ int main(int argc, char **argv)
     plan plan11;
     plan plan12;
     
-    plan1.pt1 = &tabPoint[0];
-    plan1.pt2 = &tabPoint[1];
-    plan1.pt3 = &tabPoint[2];
+    plan1.pt1 = &model[0];
+    plan1.pt2 = &model[1];
+    plan1.pt3 = &model[2];
     plan1.color.r = 255;
     plan1.color.g = 0;
     plan1.color.b = 0;
     
-    plan2.pt1 = &tabPoint[2];
-    plan2.pt2 = &tabPoint[3];
-    plan2.pt3 = &tabPoint[0];
+    plan2.pt1 = &model[2];
+    plan2.pt2 = &model[3];
+    plan2.pt3 = &model[0];
     plan2.color.r = 255;
     plan2.color.g = 0;
     plan2.color.b = 0;
     
-    plan3.pt1 = &tabPoint[4];
-    plan3.pt2 = &tabPoint[5];
-    plan3.pt3 = &tabPoint[6];
+    plan3.pt1 = &model[4];
+    plan3.pt2 = &model[5];
+    plan3.pt3 = &model[6];
     plan3.color.r = 0;
     plan3.color.g = 0;
     plan3.color.b = 255;
     
-    plan4.pt1 = &tabPoint[6];
-    plan4.pt2 = &tabPoint[7];
-    plan4.pt3 = &tabPoint[4];
+    plan4.pt1 = &model[6];
+    plan4.pt2 = &model[7];
+    plan4.pt3 = &model[4];
     plan4.color.r = 0;
     plan4.color.g = 0;
     plan4.color.b = 255;
     
-    plan5.pt1 = &tabPoint[5];
-    plan5.pt2 = &tabPoint[6];
-    plan5.pt3 = &tabPoint[2];
+    plan5.pt1 = &model[5];
+    plan5.pt2 = &model[6];
+    plan5.pt3 = &model[2];
     plan5.color.r = 0;
     plan5.color.g = 255;
     plan5.color.b = 0;
     
-    plan6.pt1 = &tabPoint[2];
-    plan6.pt2 = &tabPoint[1];
-    plan6.pt3 = &tabPoint[5];
+    plan6.pt1 = &model[2];
+    plan6.pt2 = &model[1];
+    plan6.pt3 = &model[5];
     plan6.color.r = 0;
     plan6.color.g = 255;
     plan6.color.b = 0;
     
-    plan7.pt1 = &tabPoint[3];
-    plan7.pt2 = &tabPoint[7];
-    plan7.pt3 = &tabPoint[6];
+    plan7.pt1 = &model[3];
+    plan7.pt2 = &model[7];
+    plan7.pt3 = &model[6];
     plan7.color.r = 127;
     plan7.color.g = 127;
     plan7.color.b = 0;
     
-    plan8.pt1 = &tabPoint[3];
-    plan8.pt2 = &tabPoint[6];
-    plan8.pt3 = &tabPoint[2];
+    plan8.pt1 = &model[3];
+    plan8.pt2 = &model[6];
+    plan8.pt3 = &model[2];
     plan8.color.r = 127;
     plan8.color.g = 127;
     plan8.color.b = 0;
     
-    plan9.pt1 = &tabPoint[3];
-    plan9.pt2 = &tabPoint[0];
-    plan9.pt3 = &tabPoint[7];
+    plan9.pt1 = &model[3];
+    plan9.pt2 = &model[0];
+    plan9.pt3 = &model[7];
     plan9.color.r = 0;
     plan9.color.g = 127;
     plan9.color.b = 127;
     
-    plan10.pt1 = &tabPoint[0];
-    plan10.pt2 = &tabPoint[4];
-    plan10.pt3 = &tabPoint[7];
+    plan10.pt1 = &model[0];
+    plan10.pt2 = &model[4];
+    plan10.pt3 = &model[7];
     plan10.color.r = 0;
     plan10.color.g = 127;
     plan10.color.b = 127;
     
-    plan11.pt1 = &tabPoint[5];
-    plan11.pt2 = &tabPoint[4];
-    plan11.pt3 = &tabPoint[0];
+    plan11.pt1 = &model[5];
+    plan11.pt2 = &model[4];
+    plan11.pt3 = &model[0];
     plan11.color.r = 127;
     plan11.color.g = 0;
     plan11.color.b = 127;
     
-    plan12.pt1 = &tabPoint[0];
-    plan12.pt2 = &tabPoint[1];
-    plan12.pt3 = &tabPoint[5];
+    plan12.pt1 = &model[0];
+    plan12.pt2 = &model[1];
+    plan12.pt3 = &model[5];
     plan12.color.r = 127;
     plan12.color.g = 0;
     plan12.color.b = 127;
@@ -449,61 +473,83 @@ int main(int argc, char **argv)
     while(1)
         //for (int k = 0; k<100; k++)
     {
-        for(int i = 0; i<8; i++)
-        {
-            rotate(0, 0, 1, angle, &tabPoint[i]);
-            rotate(0, 1, 0, angle, &tabPoint[i]);
-            rotate(1, 0, 0, angle, &tabPoint[i]);
-        }
+        Actual_time = SDL_GetTicks();
         
-        for(int i = 0; i<8; i++)
+        if(Actual_time - Previous_time > 1000/FRAME_RATE)
         {
-            tabPoint[i].depth = sqrtf(pow(camera.posX + tabPoint[i].x_3D, 2)+pow(camera.posY + tabPoint[i].y_3D, 2)+pow(camera.posZ + tabPoint[i].z_3D, 2));
-            tabPoint[i].x_2D = CONVERT_POS_X((tabPoint[i].depth*tabPoint[i].x_3D)/(tabPoint[i].depth+tabPoint[i].y_3D));
-            tabPoint[i].y_2D = CONVERT_POS_Y((tabPoint[i].depth*tabPoint[i].z_3D*(-1))/(tabPoint[i].depth+tabPoint[i].y_3D));
-        }
-        //angle += 1;
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan1);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan2);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan3);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan4);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan5);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan6);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan7);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan8);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan9);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan10);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan11);
-        draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan12);
-        
-        for(int i = 0; i<SCREEN_WIDTH; i++)
-        {
-            for(int j = 0; j<SCREEN_HEIGHT; j++)
+            Previous_time = Actual_time;
+            
+            // Moving the cube
+            for(int i = 0; i<8; i++)
             {
-                index = j*SCREEN_HEIGHT+i;
-                SDL_SetRenderDrawColor(ren, canevas[index].color.r, canevas[index].color.g, canevas[index].color.b, 255);
-                //SDL_SetRenderDrawColor(ren, canevas[index].depth, canevas[index].depth, canevas[index].depth, 255);
-                SDL_RenderDrawPoint(ren, i, j);
-                canevas[j*SCREEN_HEIGHT+i].color.r = 220;                       // We have displayed the point. Clear it!
-                canevas[j*SCREEN_HEIGHT+i].color.g = 220;                       // We have displayed the point. Clear it!
-                canevas[j*SCREEN_HEIGHT+i].color.b = 220;                       // We have displayed the point. Clear it!
-                canevas[j*SCREEN_HEIGHT+i].depth = MAX_DEPTH;
+                rotate(0, 0, 1, angle, &model[i]);
+                rotate(0, 1, 0, angle, &model[i]);
+                rotate(1, 0, 0, angle, &model[i]);
             }
+            
+            // Moving the camera
+            for(int i = 0; i<8; i++)
+            {
+                view[i] = model[i];
+               // rotate(
+                //rotate(0, 0, 1, angle, &model[i]);
+                //rotate(0, 10, 0, angle, &model[i]);
+                //rotate(1, 0, 0, angle, &model[i]);
+            }
+            
+            for(int i = 0; i<8; i++)
+            {
+                model[i].depth = sqrtf(pow(camera.posX + view[i].x_3D, 2)+pow(camera.posY + view[i].y_3D, 2)+pow(camera.posZ + view[i].z_3D, 2));
+                model[i].x_2D = CONVERT_POS_X((view[i].depth*view[i].x_3D)/(view[i].depth+view[i].y_3D));
+                model[i].y_2D = CONVERT_POS_Y((view[i].depth*view[i].z_3D*(-1))/(view[i].depth+view[i].y_3D));
+            }
+            //angle += 1;
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan1);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan2);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan3);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan4);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan5);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan6);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan7);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan8);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan9);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan10);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan11);
+            draw2DTriangle((m3DPoint*)canevas, SCREEN_WIDTH, SCREEN_HEIGHT, &plan12);
+            
+            for(int i = 0; i<SCREEN_WIDTH; i++)
+            {
+                for(int j = 0; j<SCREEN_HEIGHT; j++)
+                {
+                    index = j*SCREEN_HEIGHT+i;
+                    SDL_SetRenderDrawColor(ren, canevas[index].color.r, canevas[index].color.g, canevas[index].color.b, 255);
+                    //SDL_SetRenderDrawColor(ren, canevas[index].depth, canevas[index].depth, canevas[index].depth, 255);
+                    SDL_RenderDrawPoint(ren, i, j);
+                    canevas[j*SCREEN_HEIGHT+i].color.r = 220;                       // We have displayed the point. Clear it!
+                    canevas[j*SCREEN_HEIGHT+i].color.g = 220;                       // We have displayed the point. Clear it!
+                    canevas[j*SCREEN_HEIGHT+i].color.b = 220;                       // We have displayed the point. Clear it!
+                    canevas[j*SCREEN_HEIGHT+i].depth = MAX_DEPTH;
+                }
+            }
+            for (int i = 0; i<8; i++)
+            {
+                sprintf(msg_coord_pt[i], "X: %.1lf, Y: %.1lf, Z: %.1lf, D: %.1lf", model[i].x_3D, model[i].y_3D, model[i].z_3D, model[i].depth);
+                //sprintf(msg_coord_pt[i], "%lf", model[i].depth);
+                Surf_coord_pt[i] = TTF_RenderText_Solid(police, msg_coord_pt[i], black);
+                Rect_coord_pt[i].x =  view[i].x_2D;
+                Rect_coord_pt[i].y =  view[i].y_2D;
+                text_coord_pt[i] = SDL_CreateTextureFromSurface(ren, Surf_coord_pt[i]);
+                SDL_FreeSurface(Surf_coord_pt[i]);
+                SDL_RenderCopy(ren, text_coord_pt[i], NULL, &Rect_coord_pt[i]);
+                SDL_DestroyTexture(text_coord_pt[i]);
+            }
+            SDL_RenderPresent(ren);
+            SDL_Delay(1000/FRAME_RATE);
         }
-        for (int i = 0; i<8; i++)
+        else
         {
-            sprintf(msg_coord_pt[i], "X: %.1lf, Y: %.1lf, Z: %.1lf, D: %.1lf", tabPoint[i].x_3D, tabPoint[i].y_3D, tabPoint[i].z_3D, tabPoint[i].depth);
-            //sprintf(msg_coord_pt[i], "%lf", tabPoint[i].depth);
-            Surf_coord_pt[i] = TTF_RenderText_Solid(police, msg_coord_pt[i], black);
-            Rect_coord_pt[i].x =  tabPoint[i].x_2D;
-            Rect_coord_pt[i].y =  tabPoint[i].y_2D;
-            text_coord_pt[i] = SDL_CreateTextureFromSurface(ren, Surf_coord_pt[i]);
-            SDL_FreeSurface(Surf_coord_pt[i]);
-            SDL_RenderCopy(ren, text_coord_pt[i], NULL, &Rect_coord_pt[i]);
-            SDL_DestroyTexture(text_coord_pt[i]);
+            SDL_Delay(Actual_time - Previous_time - 1000/FRAME_RATE);
         }
-        SDL_RenderPresent(ren);
-        SDL_Delay(1000/FRAME_RATE);
     }
     pause();
     SDL_DestroyRenderer(ren);
