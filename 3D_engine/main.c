3971d2f19b6fac6d217f4d39a493cb7f88cb32de
SerialLooser <haroldducept@gmail.com>
2016-12-24 19:33:30 +0100

    Creation of threads successful.

--- 3D_engine/main.c
+++ 3D_engine/main.c
@@ -1,12 +1,59 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <pthread.h>
+
+static void *thread_userEvent(void *p_data)
+{
+    while(1)
+    {
+        printf("thread event\n");
+    }
+}
+
+static void *thread_3DWorld(void *p_data)
+{
+    while(1)
+    {
+        printf("thread 3D world\n");
+    }
+}
+
+static void *thread_3Dto2DWorld(void *p_data)
+{
+    while(1)
+    {
+        printf("thread 3D to 2D world\n");
+    }
+}
+
+static void *thread_fillTriangles(void *p_data)
+{
+    while(1)
+    {
+        printf("thread fill triangle\n");
+    }
+}
+
+static void *thread_painter(void *p_data)
+{
+    while(1)
+    {
+        printf("thread painter\n");
+    }
+}
 
 int main(int argc, char **argv)
 {
     SDL_Window *window = NULL;
     SDL_Event event;
     bool end;
+    int8_t ret = 0;
+    pthread_t threadT_userEvent;
+    pthread_t threadT_3DWorld;
+    pthread_t threadT_3Dto2DWorld;
+    pthread_t threadT_fillTriangles;
+    pthread_t threadT_painter;
     
     // Initialisation de la SDL
     
@@ -26,6 +73,29 @@ int main(int argc, char **argv)
         SDL_Quit();
         return -1;
     }
+    
+    // Threads creation
+    // Creation of user events thread
+    ret = pthread_create(&threadT_userEvent, NULL, thread_userEvent, NULL);
+    // Creation of 3D world thread
+    ret = pthread_create(&threadT_3DWorld, NULL, thread_3DWorld, NULL);
+    // Creation of 3D to 2D world thread
+    ret = pthread_create(&threadT_3Dto2DWorld, NULL, thread_3Dto2DWorld, NULL);
+    // Creation of fill triangles thread
+    ret = pthread_create(&threadT_fillTriangles, NULL, thread_fillTriangles, NULL);
+    // Creation of painter thread
+    ret = pthread_create(&threadT_painter, NULL, thread_painter, NULL);
+    
+    if(ret == 0)
+    {
+        printf("thread creation successfull\n");
+    }
+    else
+    {
+        printf("thread creation failed\n");
+        end = false;
+    }
+    
     while(!end)
     {
         SDL_WaitEvent(&event);
